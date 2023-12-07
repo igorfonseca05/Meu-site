057527f1cc0ab748b905c5287d5f78601fc13758
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2023-12-07 15:04:30 -0300

    modify card

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const externalMenuIcon = document.querySelector('[data-js="externalIcon"]')
 const internalMenuIcon = document.querySelector('[data-js="internalIcon"]')
 const menuMobile = document.querySelector('[data-js="menuMobile"]')
 const backgroundMenu = document.querySelector('[data-js="darkenBackground"]')
-
+const card = document.querySelector('.card')
 
 const openMenu = () => {
     backgroundMenu.style.display = 'block'
@@ -16,6 +16,11 @@ const closeMenu = () => {
     menuMobile.classList.add('closeMenu')
 }
 
+const turnCard = () => {
+    card.classList.toggle('is-flipped')
+}
+
 externalMenuIcon.addEventListener('click', openMenu)
 internalMenuIcon.addEventListener('click', closeMenu)
 backgroundMenu.addEventListener('click', closeMenu)
+card.addEventListener('click',turnCard)
