a389852fb89567fac0b5e27c10cf11639e0c7c6b
https://github.com/igorfonseca05 <78857547+igorfonseca05@users.noreply.github.com>
2023-12-08 22:34:25 -0300

    desktop style

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const backgroundMenu = document.querySelector('[data-js="darkenBackground"]')
 const ulContainer = [...document.querySelector('[data-js="ulContainer"]').children]
 
 
+
 const openMenu = () => {
     backgroundMenu.style.display = 'block'
     menuMobile.classList.remove('closeMenu')
@@ -29,21 +30,45 @@ const getGitHubDatas = () =>
         .then(res => res.json())
         .catch(console.log)
 
+
 const showDGitDatasOnScreen = async () => {
     const amountProject =  document.querySelector('[data-js="reposNum"]')
     const mainText = document.querySelector('[data-js="aboutMePragraph"]')
     const dadosGit = await getGitHubDatas()
-
-    console.log(dadosGit)
-
-    mainText.textContent = dadosGit.bio
-    amountProject.textContent = dadosGit.public_repos
-
     
+    mainText.textContent = dadosGit.bio
+    // amountProject.textContent = dadosGit.public_repos 
+    
+    addAnimationcount(amountProject , dadosGit.public_repos )
+}
+
+const addAnimationcount = (numContainer, amountProjects) => {
+    const timer = setInterval(() => {
+       const value = ++numContainer.textContent
+       
+       if(value === amountProjects) {
+           clearInterval(timer)
+           return
+       }
+    }, 70)
 }
 
 
 
+const addHoverOnButton = () => {
+    const buttons = document.querySelectorAll('[data-js="hoverable"]')
+
+   console.log(buttons[0].style)
+
+   buttons.forEach(button => {
+    button.addEventListener('touchstart', () => {
+        button.style.backgroundColor = 'rgb(0, 229, 255)';
+        button.style.boxShadow = '0px 0px 3px 1px rgb(0, 229, 255)'
+    })
+})
+}
+
+addHoverOnButton()
 
 externalMenuIcon.addEventListener('click', openMenu)
 internalMenuIcon.addEventListener('click', closeMenu)
